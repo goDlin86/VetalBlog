04b0ceccf4d312da2469c693488c91bddcc31892
goDlin <godlin086@gmail.com>
2019-11-11 18:18:59 +0300

    upload image

--- VetalBlog/settings.py
+++ VetalBlog/settings.py
@@ -143,7 +143,10 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-MEDIA_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = 'https://%s.s3.eu-central-1.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_HOST = 's3.eu-central-1.amazonaws.com'
+S3_USE_SIGV4 = True
+AWS_S3_REGION_NAME = 'eu-central-1'
 
 django_heroku.settings(locals())
 
